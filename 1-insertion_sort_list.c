b1ccbb620b68d55170f691da270196677dc045d4
Modibo Keita <modibokeita9390@gmail.com>
2024-01-18 12:11:02 -0800

    1-insertion_sort_list.c

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,60 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list
+ * of integers in ascending order using the
+ * Insertion sort
+ * @list: lsit to be sorted
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp;
+
+	if (list == NULL || (*list)->next == NULL)
+		return;
+
+	temp = (*list)->next;
+
+	while (temp)
+	{
+		while ((temp->prev) && (temp->prev->n > temp->n))
+		{
+			temp = swap_number(temp, list);
+
+			print_list(*list);
+		}
+		temp = temp->next;
+	}
+}
+
+/**
+ * swap_number - checks each index of the list then
+ * compare them if next index is greater than
+ * prev index then swap
+ * @list: list to be checked
+ * @temp: stores value temporary
+ * Return: sorted list
+ */
+
+listint_t *swap_number(listint_t *temp, listint_t **list)
+{
+	listint_t *decreament = temp->prev, *current = temp;
+
+	decreament->next = current->next;
+
+	if (current->next)
+		current->next->prev = decreament;
+
+	current->next = decreament;
+	current->prev = decreament->prev;
+
+	decreament->prev = current;
+
+	if (current->prev)
+		current->prev->next = current;
+
+	else
+		*list = current;
+	return (current);
+}
