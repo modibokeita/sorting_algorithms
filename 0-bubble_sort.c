e9a166e1c74afde3562d8bcf19ac98c5cb481fb0
Modibo Keita <modibokeita9390@gmail.com>
2024-01-19 07:00:57 -0800

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,11 +15,11 @@ void bubble_sort(int *array, size_t size)
 
 	int temp;
 
-	int swap;
+	if (array == NULL  || size < 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swap = 0;
 
 		for (j = 0; j < size - 1 - i; j++)
 		{
@@ -30,12 +30,9 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 
 				array[j + 1] = temp;
-				swap = 1;
 
 				print_array(array, size);
 			}
 		}
-		if (swap == 0)
-			break;
 	}
 }
