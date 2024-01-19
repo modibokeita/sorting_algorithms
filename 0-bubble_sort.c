d2723f4a2da9c0ffa3e22a3a8f5853fce41cff68
Modibo Keita <modibokeita9390@gmail.com>
2024-01-19 07:17:54 -0800

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,18 +1,18 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sorts an array of integers in
- * ascending order using the Bubble sort algorithm
- * @array: Array to be sorted
- * @size: the size of array
+ *bubble_sort - sorts an array
+ *@array: array to be sorted
+ *@size: Size of array
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t j, i;
 	int tmp;
 
 	if (array == NULL  || size < 2)
 		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
@@ -20,8 +20,10 @@ void bubble_sort(int *array, size_t size)
 			if (array[j] > array[j + 1])
 			{
 				tmp = array[j];
+
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
+
 				print_array(array, size);
 			}
 		}
